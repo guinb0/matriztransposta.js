aba9546e90ac2f22e6c03b95598a7c8793d9a123
guinb0 <128229850+guinb0@users.noreply.github.com>
2024-03-27 12:59:23 -0300

    Create matrizTransposta.js

---
+++ matrizTransposta.js
@@ -0,0 +1,21 @@
+function transporMatriz(A) {
+    // Imprime a matriz original
+    console.log("Matriz Original:");
+    A.forEach(linha => console.log(linha.join(' ')));
+    
+   
+    const transposta = Array.from({ length: A[0].length }, () => Array(A.length).fill(0));
+    for (let i = 0; i < A.length; i++) {
+        for (let j = 0; j < A[0].length; j++) {
+            transposta[j][i] = A[i][j];
+        }
+    }
+
+   
+    console.log("\nMatriz Transposta:");
+    transposta.forEach(linha => console.log(linha.join(' ')));
+}
+
+
+const matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+transporMatriz(matriz);
